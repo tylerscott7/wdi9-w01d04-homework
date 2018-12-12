d455e5a68345c6c3469f34cf703b00b9922fbb22
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2018-12-12 12:00:09 -0700

    Commit 5 - Sum Array

--- homework_3/js/app.js
+++ homework_3/js/app.js
@@ -61,3 +61,15 @@ function calculateSide (sideA, sideB){
 }
 
 console.log(calculateSide(8,6));
+
+// Problem 5
+function sumArray (arr){
+    // Iterate through and add the numbers
+    let sum = 0
+    for (let i = 0; i<arr.length; i++){
+        sum += arr[i];
+    };
+    return sum;
+};
+
+console.log(sumArray([1, 2, 3, 4, 5, 6]));
