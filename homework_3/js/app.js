360a006c1b4918ed1200ea2458f62990469cf6c1
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2018-12-12 10:31:48 -0700

    Commit 1 - Verbal questions

---
+++ homework_3/js/app.js
@@ -0,0 +1,19 @@
+// Problem 1
+// 1. A parameter is a value that a function
+//    receives in order to work correctly.
+//
+//    An argument is what the user inputs into
+//    a method to have the method/function work
+//    with their specific argument.
+// 
+// 2. Return will spit out a value within JavaScript
+//    that the user can work with in their project whereas
+//    console.log prints values to the console in order
+//    to more efficiently troubleshoot problems.
+//
+// 3. Since a function can return a value, it allows your
+//    code to be "DRY" because instead of trying to 
+//    re-evaluate the same thing multiple times, you can
+//    simply wrap the action in a function to call on.
+//
+// 
